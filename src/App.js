b987b0ccd4da8572b250251467501376f6c6a782
ferrari219@nate.com <ferrari219@nate.com>
2021-03-16 11:42:29 +0900

    기본 환경 셋팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import style from 'scss/index';
 class App extends Component {
 	render() {
 		return <div>Linking</div>;
---
+++ src/Scss/index.js
@@ -0,0 +1,5 @@
+//Global SCSS
+import './color.scss';
+import './font.scss';
+import './grid.scss';
+import './common.scss';
